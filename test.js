956c5c6b37e23523c5144d5bbb5473de866be855
Ssegura09 <stephaniemsegura09@gmail.com>
2021-02-24 17:12:50 -0600

    add up to

--- test.js
+++ test.js
@@ -74,3 +74,12 @@ function TheseThreeFour() {
     </div>
   );
 }
+
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
