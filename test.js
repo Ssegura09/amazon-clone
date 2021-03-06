13c832ac31e24a42d4085fa3cf9a19e9af270d95
Ssegura09 <stephaniemsegura09@gmail.com>
2021-03-06 15:55:05 -0600

    Dona a.

--- test.js
+++ test.js
@@ -94,3 +94,12 @@ addUpTo(1000000000);
 var t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
 //best option!
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+//best option!
